7c057ed832acf4edef9124a80ec33865a4cbbf0d
Sriraam Prasad <selvansriram373@gmail.com>
2026-01-27 16:15:16 +0530

    Add files via upload

--- nlp2.py
+++ nlp2.py
@@ -1,19 +1,27 @@
+import nltk
+nltk.download('punkt_tab')
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
-from nltk.tokenize import work_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, SnowballStemmer, LancasterStemmer, RegexpStemmer
+text = """the product quality is very good and i am extremely happy with the service.
+the delivery was late, but the customer support was helpful.
+overall, the experience was positive and i would recommend this company."""
+words = [
+    w for w in word_tokenize(text.lower())
+    if w.isalpha() and w not in stopwords.words('english')]
 
-text = """The product quality is very good and I am extremely happy with the service.
-            The delivery was late, but the customer supportes was helpful.
-            Overall, the experience was positive and I would recommended this company."""
-
-words=[w for w in word.tokenize(text.lower()) if w.usalpha() and w not in stopwords.words('english')]
-print("Words after Stop words removal\n", words)
-
+print("Words after stop words removal:\n", words)
 porter = PorterStemmer()
-snowball = SnowballStemmer("english")
-lancaster = LancasterStemmer()
+sb = SnowballStemmer("english")
+lc = LancasterStemmer()
 regex = RegexpStemmer('ing$|ed$|ly$|s$|es$')
-print("\nPorter Stemmer :", [porter.stem(w) for w in words])
-print("\nSnowball Stemmer :",[snowball.stem(w) for w in words])
-print("\nLancaster Stemmer:",[lancaster.stem(w) for w in words])
-print("\nRegexp Stemmer:",[regex.stem(w) for w in words])
+print("\nPorter Stemmer")
+print([porter.stem(w) for w in words])
+print("\nSnowball Stemmer")
+print([sb.stem(w) for w in words])
+print("\nLancaster Stemmer")
+print([lc.stem(w) for w in words])
+print("\nRegex Stemmer")
+print([regex.stem(w) for w in words])
